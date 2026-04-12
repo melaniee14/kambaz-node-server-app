f489e4f384c240b3ee5b33c5e39ad05e983aafa2
melaniee14 <hammond.me@northeastern.edu>
2026-04-12 17:45:43 -0400

    Hellooooo

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -4,11 +4,11 @@ export default function EnrollmentsDao() {
 
   async function findCoursesForUser(userId) {
     const enrollments = await model.find({ user: userId }).populate("course");
-    return enrollments.map((enrollment) => enrollment.course);
+    return enrollments.map((enrollment) => enrollment.course).filter(Boolean);
   }
   async function findUsersForCourse(courseId) {
     const enrollments = await model.find({ course: courseId }).populate("user");
-    return enrollments.map((enrollment) => enrollment.user);
+    return enrollments.map((enrollment) => enrollment.user).filter(Boolean);
   }
  
   function enrollUserInCourse(userId, courseId) {
